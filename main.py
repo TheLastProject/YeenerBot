8eb230350342efa2760e3cf300c6d2465519a010
Sylvia van Os <sylvia@hackerchick.me>
2018-09-13 22:31:19 +0200

    Oh crap

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def resolve_chat(function):
                 if chat.type == 'private':
                     continue
 
-                if superadmin and not chat.get_member(user.id).status in ['creator', 'administrator', 'member']:
+                if not superadmin and not chat.get_member(user.id).status in ['creator', 'administrator', 'member']:
                     continue
 
                 chats.append(chat)
